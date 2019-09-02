829db73056b3028c4916ab3485dd050e36846769
Arya <arya721@gmail.com>
2019-09-02 03:19:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,10 @@
 def using_include(array, element)
-  famous_cats = ["lil' bub", "grumpy", "Maru"]
-famous_cats.include?("grumpy")
+  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+  famous_cats.include?("Garfield")
+    => false
+  famous_cats.include?("Maru")
+    => true
+
 end
 
 def using_sort(array)
